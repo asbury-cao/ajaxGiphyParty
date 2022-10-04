a1ce3a273fba0b441a6d2858425ec8197494efc3
Asbury Cao <asbury.cao@gmail.com>
2022-10-04 16:32:44 -0700

    added async function and showGif and deleteGifs functions with event listeners

--- ajax-giphy-party/app.js
+++ ajax-giphy-party/app.js
@@ -1,1 +1,32 @@
+"use strict";
+
 console.log("Let's get this party started!");
+
+
+async function getGif(evt) {
+  evt.preventDefault();
+  const inputVal = $('input').val();
+  const apiKey = 'MhAodEJIJxQMxW9XqxKjyXfNYdLoOIym';
+  const response = await axios.get('http://api.giphy.com/v1/gifs/search',
+    { params: { q: inputVal, api_key: apiKey } });
+
+
+  const gif = response.data.data[0].images.downsized.url;
+  showGif(gif);
+  console.log('i was clicked');
+}
+
+$('#submit-button').on('click', getGif);
+
+function showGif(gifURL) {
+  let $image = $('<img></img>');
+  $image.attr('src', gifURL);
+  $('#gif-container').append($image);
+}
+
+
+$('#delete-button').on('click', deleteGifs);
+
+function deleteGifs() {
+  $('#gif-container').empty();
+}
