edbfcaa02613cda9fa63823852960bee9254c98e
Ashley Mathew <amathew195@gmail.com>
2022-10-04 18:37:46 -0500

    updated docstrings

--- ajax-giphy-party/app.js
+++ ajax-giphy-party/app.js
@@ -2,7 +2,10 @@
 
 console.log("Let's get this party started!");
 
-
+/** getGif: stays on the same page, upon being invoked. Takes the value from
+ * the search input and the api key to send a GET request to giphy API. It
+ * stores the URL for the gif and passes to the showGif function.
+ */
 async function getGif(evt) {
   evt.preventDefault();
   const inputVal = $('input').val();
@@ -18,6 +21,10 @@ async function getGif(evt) {
 
 $('#submit-button').on('click', getGif);
 
+/**
+ * showGif: intakes the gifURL and adds the URL as a source to a new image that
+ * is appended to the gif container.
+ */
 function showGif(gifURL) {
   let $image = $('<img></img>');
   $image.attr('src', gifURL);
@@ -27,6 +34,9 @@ function showGif(gifURL) {
 
 $('#delete-button').on('click', deleteGifs);
 
+/**
+ * deleteGifs: empties the gif container
+ */
 function deleteGifs() {
   $('#gif-container').empty();
 }
