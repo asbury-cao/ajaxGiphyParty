d3d74c16aab0cbf70e0f46bf17f248046140fc33
Ashley Mathew <amathew195@gmail.com>
2022-10-04 20:26:27 -0500

    added controller function

--- ajax-giphy-party/app.js
+++ ajax-giphy-party/app.js
@@ -6,28 +6,30 @@ console.log("Let's get this party started!");
  * the search input and the api key to send a GET request to giphy API. It
  * stores the URL for the gif and passes to the showGif function.
  */
-async function getGif(evt) {
-  evt.preventDefault();
+async function getGif() {
   const inputVal = $('input').val();
-  const apiKey = 'MhAodEJIJxQMxW9XqxKjyXfNYdLoOIym';
+  const apiKey = 'MhAodEJIJxQMxW9XqxKjyXfNYdLoOIym'; //TODO: MAKE GLOBAL CONST, SO YOU CAN ACCESS THROUGHOUT THE APPLICATION
   const response = await axios.get('http://api.giphy.com/v1/gifs/search',
     { params: { q: inputVal, api_key: apiKey } });
 
 
-  const gif = response.data.data[0].images.downsized.url;
-  showGif(gif);
-  console.log('i was clicked');
+  return response.data.data[0].images.downsized.url;
 }
 
-$('#submit-button').on('click', getGif);
+async function handleClick(evt) {
+  evt.preventDefault();
+  const url = await getGif();
+  showGif(url);
+}
+
+$('#submit-button').on('click', handleClick);
 
 /**
  * showGif: intakes the gifURL and adds the URL as a source to a new image that
  * is appended to the gif container.
  */
 function showGif(gifURL) {
-  let $image = $('<img></img>');
-  $image.attr('src', gifURL);
+  let $image = $('<img>').attr('src', gifURL);
   $('#gif-container').append($image);
 }
 
